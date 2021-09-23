7d7dc21af3640568793e804f5dac0ef53d2a74bc
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-09-23 14:58:30 -0500

    completed steps 1-4 of jpa exercise

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,13 +1,42 @@
 package com.codeup.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
     private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String body;
+
+    public Post() {
+
+    }
 
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
+    }
+
+    public Post(Long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
