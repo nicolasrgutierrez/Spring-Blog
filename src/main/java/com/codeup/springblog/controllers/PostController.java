3135280213e533fd5dd506b2ed4fe3d027c67ffe
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-09-29 12:30:53 -0500

    added email service, need to test

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -3,12 +3,12 @@ package com.codeup.springblog.controllers;
 import com.codeup.springblog.models.Post;
 import com.codeup.springblog.repos.PostRepository;
 import com.codeup.springblog.repos.UserRepository;
+import com.codeup.springblog.services.EmailService;
 import org.apache.coyote.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -16,51 +16,63 @@ public class PostController {
 
     private final PostRepository postDao;
     private final UserRepository userDao;
+    private final EmailService emailService;
 
-    public PostController(PostRepository postDao, UserRepository userDao) {
+    public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService) {
         this.postDao = postDao;
         this.userDao = userDao;
+        this.emailService = emailService;
     }
 
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    public String indexPage(Model model) {
+
+    @GetMapping("/posts")
+    public String showPosts(Model model) {
         List<Post> allPosts = postDao.findAll();
         model.addAttribute("posts", allPosts);
         return "post/index";
     }
 
-    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    public String viewPost(@PathVariable long id, Model model) {
+    @GetMapping("/posts/{id}")
+    public String showOnePost(@PathVariable long id, Model model) {
         Post post = postDao.getById(id);
+        model.addAttribute("postId", id);
         model.addAttribute("post", post);
         return "post/show";
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
-    public String showCreateForm(Model model) {
+    public String showCreatePostForm(Model model) {
         model.addAttribute("post", new Post());
-        return "posts/create";
+        return "post/create";
     }
+
     @PostMapping("/posts/create")
-    @ResponseBody
-    public String createPost(@ModelAttribute Post post) {
-        post.setOwner(userDao.getById(1L));
+    public String createPost(@ModelAttribute Post postToAdd) {
+
+        postToAdd.setOwner(userDao.getById(1L));
+
+        postDao.save(postToAdd);
         return "redirect:/posts";
     }
 
     @GetMapping("/posts/edit/{id}")
-    public String showEditForm(@PathVariable long id, Model model) {
-        Post post = postDao.getById(id);
-        model.addAttribute("post", post);
-        return "/post/edit";
+    public String showEditPostForm(@PathVariable long id, Model model) {
+        Post postToEdit = postDao.getById(id);
+        model.addAttribute("postToEdit",postToEdit);
+        return "post/edit";
     }
 
     @PostMapping("/posts/edit/{id}")
-    public String editPost(@ModelAttribute Post post) {
-        postDao.save(post);
+    public String editPost(
+            @PathVariable long id,
+            @ModelAttribute Post updatedPost
+    ) {
+        updatedPost.setId(id);
+        updatedPost.setOwner(userDao.getById(1L));
+        postDao.save(updatedPost);
 
         return "redirect:/posts";
+
     }
 
     @PostMapping("/posts/delete/{id}")
@@ -69,6 +81,7 @@ public class PostController {
         postDao.delete(postToDelete);
 
         return "redirect:/posts";
+
     }
 
 }
